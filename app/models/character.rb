7ec4560b25d197bb41dc46d6c28b4be058c7a668
Blaire <grant.blaire@gmail.com>
2021-06-27 09:25:04 -0500

    Updated routes.rb file and characters_controller to include search_by_name and search_by_id actions. Added character_by_name and character_by_id class methods to character model which will use the Marvel API to pull specified character's inforamtion.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,14 @@ class Character < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def self.character_by_name(name, ts, hash)
+    response = RestClient.get("http://gateway.marvel.com/v1/public/characters?nameStartsWith=#{name}&ts=#{ts}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{hash}")
+    character_response = JSON.parse(response)
+  end
+
+  def self.character_by_id(id, ts, hash)
+    response = RestClient.get("http://gateway.marvel.com/v1/public/characters/#{id}?&ts=#{ts}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{hash}")
+    character_response = JSON.parse(response)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  post '/search_by_name', to: 'characters#search_by_name'
+  post '/search_by_id', to: 'characters#search_by_id'
+  resources :characters
+
   resources :comments
   resources :likes
   resources :users
-  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
