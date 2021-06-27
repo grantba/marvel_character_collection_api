6fa05cf2fe1cda89de34972dd67e01af8ce5fa65
Blaire <grant.blaire@gmail.com>
2021-06-27 09:06:25 -0500

    Added validations to user and comment models.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :character
+
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :characters
     has_many :comments
     has_many :likes
+
+    validates :username, presence: true
+    validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
 end
