5606052df24b962554cbd646071b4139d5c46186
Blaire <grant.blaire@gmail.com>
2021-06-27 09:16:37 -0500

    Updated comments_controller to render data using the models serializer class.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,39 +3,42 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    @comments = Comment.all
-
-    render json: @comments
+    comments = Comment.all
+    render json: CommentSerializer.new(comments)
   end
 
   # GET /comments/1
   def show
-    render json: @comment
+    render json: CommentSerializer.new(@comment)
   end
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
-
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: CommentSerializer.new(comment), status: :created, location: comment
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: CommentSerializer.new(comment).errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /comments/1
   def update
     if @comment.update(comment_params)
-      render json: @comment
+      render json: CommentSerializer.new(@comment)
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: CommentSerializer.new(@comment).errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /comments/1
   def destroy
-    @comment.destroy
+    if @comment
+      @comment.destroy
+      render json: CommentSerializer.new(@comment)
+    else
+      render json: CommentSerializer.new(@comment).errors, status: :unprocessable_entity
+    end   
   end
 
   private
@@ -46,6 +49,7 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:description, :user_id, :character_id)
+      params.require(:comment).permit(:description, :user_id, :comment_id)
     end
+  end
 end
